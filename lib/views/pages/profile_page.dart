22a8be51c21158453a871e76cfd38c0cac4f1380
summydev <adegbitesumayah@gmail.com>
2024-10-17 11:19:19 +0100

    profilePage display fixed

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -1,4 +1,6 @@
+import 'package:finstagram/model/services/service.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 
 class ProfilePage extends StatefulWidget {
@@ -9,10 +11,46 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  FirebaseService? _firebaseService;
+  double? _deviceHeight, _deviceWidth;
+
+  @override
+  void initState() {
+
+    super.initState();
+    _firebaseService = GetIt.instance.get<FirebaseService>();
+  }
   @override
   Widget build(BuildContext context) {
+    _deviceWidth =MediaQuery.of(context).size.width;
+    _deviceHeight =MediaQuery.of(context).size.height;
     return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: _deviceWidth! * 0.05,
+        vertical: _deviceHeight! * 0.02
+      ),
+
       color: Colors.orange,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          _profileImage(),
+        ],
+
+      ),
+    );
+  }
+  Widget _profileImage(){
+    return Container(
+      margin: EdgeInsets.only(bottom: _deviceHeight! * 0.02),
+      height: _deviceHeight! * 0.15,
+      width: _deviceHeight! * 0.15,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100.0),
+        image: DecorationImage(fit: BoxFit.cover, image: NetworkImage(_firebaseService!.currentUser!["image"]))
+      ),
     );
   }
 }
